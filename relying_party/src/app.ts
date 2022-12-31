f911de16b4877aeff0bddea8bf7143f4dade69b0
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2022-12-31 18:25:31 +0900

    add 'express' template files

---
+++ relying_party/src/app.ts
@@ -0,0 +1,36 @@
+import createHttpError from "http-errors";
+import express, { Request, Response, NextFunction } from "express";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+
+import { router as indexRouter } from "./routes/index";
+
+const app = express();
+
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static("./public"));
+
+app.use("/", indexRouter);
+
+app.use((req: Request, res: Response, next: NextFunction) =>
+    next(createHttpError(404))
+);
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+
+    res.status(err.status || 500);
+    res.render("error");
+});
+
+app.listen(3000,()=>{
+    console.log('start port to 3000')
+});
+
+module.exports = app;
