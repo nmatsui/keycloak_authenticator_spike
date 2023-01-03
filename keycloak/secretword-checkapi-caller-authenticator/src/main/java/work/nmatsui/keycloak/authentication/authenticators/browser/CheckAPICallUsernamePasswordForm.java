06cd369fae98b0c7fffb3336a73acfdfe052b7b0
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2023-01-03 16:14:32 +0900

    implement 'checkSecretWord' method in CheckAPICallUsernamePasswordForm to check the secret word by calling checker api

--- keycloak/secretword-checkapi-caller-authenticator/src/main/java/work/nmatsui/keycloak/authentication/authenticators/browser/CheckAPICallUsernamePasswordForm.java
+++ keycloak/secretword-checkapi-caller-authenticator/src/main/java/work/nmatsui/keycloak/authentication/authenticators/browser/CheckAPICallUsernamePasswordForm.java
@@ -1,5 +1,12 @@
 package work.nmatsui.keycloak.authentication.authenticators.browser;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Optional;
+
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
 
@@ -13,24 +20,57 @@ public class CheckAPICallUsernamePasswordForm extends UsernamePasswordForm {
   private enum CustomMessages {
     SECRET_WORD_DOES_NOT_MATCH
   }
+  private static final String USERNAME_KEY = "username";
+  private static final String SECRETWORD_KEY = "secretWord";
+
+  private static final String CHECK_API_ENDPOINT = Optional.ofNullable(System.getenv("CHECK_API_ENDPOINT"))
+      .orElse("http://localhost:3000/users/%s/check/");
+  private static final String CHECK_API_SECRETWORD_KEY = Optional.ofNullable(System.getenv("CHECK_API_SECRETWORD_KEY"))
+      .orElse("secretWord");
+
+  private boolean checkSecretWord(AuthenticationFlowContext context, MultivaluedMap<String, String> formData) {
+    String username = formData.getFirst(USERNAME_KEY);
+    String secretWord = formData.getFirst(SECRETWORD_KEY);
+    String msg = String.format("{\"%s\":\"%s\"}", CHECK_API_SECRETWORD_KEY, secretWord);
+    LOG.infof("msfg: %s", msg);
+
+    HttpClient client = HttpClient.newHttpClient();
+    HttpRequest request = HttpRequest.newBuilder(
+        URI.create(String.format(CHECK_API_ENDPOINT, username)))
+        .header("content-type", "application/json")
+        .PUT(HttpRequest.BodyPublishers.ofString(msg))
+        .build();
+    try {
+      HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+      int statusCode = response.statusCode();
+      if (200 <= statusCode && statusCode < 300) {
+        LOG.infof("check success: %s, %d", response.body(), statusCode);
+        return true;
+      } else {
+        LOG.warnf("check failure: %s, %d", response.body(), statusCode);
+      }
+    } catch (IOException | InterruptedException e) {
+      e.printStackTrace();
+      LOG.errorf("check api throws an eception: %s", e.getMessage());
+    }
+
+    Response challenge = context.form().setError(CustomMessages.SECRET_WORD_DOES_NOT_MATCH.name()).createLoginUsernamePassword();
+    context.failureChallenge(AuthenticationFlowError.INVALID_CREDENTIALS, challenge);
+    return false;
+  }
 
   @Override
   public void action(AuthenticationFlowContext context) {
     LOG.infof("action is called: context=%s", context);
     MultivaluedMap<String, String> formData = context.getHttpRequest().getDecodedFormParameters();
     if (formData.containsKey("cancel")) {
-        context.cancelLogin();
-        return;
+      context.cancelLogin();
+      return;
     }
     if (!validateForm(context, formData)) {
-        return;
+      return;
     }
-
-    String secretWord = formData.getFirst("secretWord");
-    LOG.infof("secretWord: %s", secretWord);
-    if (!"checkapi".equals(secretWord)) {
-      Response challenge =  context.form().setError(CustomMessages.SECRET_WORD_DOES_NOT_MATCH.name()).createLoginUsernamePassword();
-      context.failureChallenge(AuthenticationFlowError.INVALID_CREDENTIALS, challenge);
+    if (!checkSecretWord(context, formData)) {
       return;
     }
 
